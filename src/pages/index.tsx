d3cd831207f3ebe7c7b802beb0152c53de47e034
Franck LEBAS <francklebas@protonmail.ch>
2021-07-03 13:31:39 +0200

    feat: homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Layout from "../components/Layout";
 import BasicMeta from "../components/meta/BasicMeta";
 import OpenGraphMeta from "../components/meta/OpenGraphMeta";
@@ -12,12 +13,33 @@ export default function Index() {
       <TwitterCardMeta url={"/"} />
       <div className="container">
         <div>
-          <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
-          </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
-          <SocialList />
+          <h1>Road to Sweden</h1>
+          <h2>
+            Le blog des pérégrinations
+            <br />
+            d'une famille d'expatriés Français
+            <br />
+            en Suède.
+          </h2>
+          <p>
+            Nous sommes Français et avons fait le choix de partir nous installer
+            en Suède.
+          </p>
+          <p>
+            Ce site raconte notre histoire au fil de l'eau. On y parle des
+            réussites liées à notre projet, mais aussi des difficultés et des
+            échecs que nous avons vécus.
+          </p>
+          <p>
+            Nous partageons des conseils pratiques sur la recherche du logement,
+            d'emploi, mais aussi sur les démarches légales afin de réussir dans
+            notre démarche.
+          </p>
+          <p>
+            Nous espérons que ce site vous servira, ou à tout le moins vous
+            divertira.
+          </p>
+          <Link href="/posts">Accéder aux articles</Link>
         </div>
       </div>
       <style jsx>{`
