9d58c03622410b2053cc58ee973c73ea6007343a
Franck LEBAS <francklebas@protonmail.ch>
2021-07-03 13:54:37 +0200

    feat: remove github

--- src/components/SocialList.tsx
+++ src/components/SocialList.tsx
@@ -1,18 +1,9 @@
 import React from "react";
-import GitHub from "../assets/github-alt.svg";
 import config from "../lib/config";
 
 export function SocialList({}) {
   return (
     <div>
-      <a
-        title="GitHub"
-        href={`https://github.com/${config.github_account}`}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <GitHub width={24} height={24} fill={"#222"} />
-      </a>
       <style jsx>{`
         a {
           display: inline-block;
